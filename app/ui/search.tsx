608fbf2c11822864a892e2a50960ed412e451343
Hassan Abbas <95937687+HassanAbbas10@users.noreply.github.com>
2024-12-17 16:27:47 +0500

    modified:   app/ui/search.tsx

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -6,15 +6,15 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  
+
   const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
-   
+
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('query', term);
+      params.set("query", term);
     } else {
-      params.delete('query');
+      params.delete("query");
     }
     replace(`${pathname}?${params.toString()}`);
   }, 300);
